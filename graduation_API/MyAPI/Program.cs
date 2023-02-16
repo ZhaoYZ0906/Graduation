23b2067f30fe51430f5a566fc19b2d5298e6cf36
zyz <1044196262@qq.com>
2023-02-16 10:58:40 +0800

    备份

---
+++ graduation_API/MyAPI/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace MyAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .MinimumLevel.Override("System", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(Path.Combine(@"log/log-.txt"),rollingInterval:RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Debug("程序启动");
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog();
+                    //本地的话可以把这个注释掉
+                    webBuilder.UseUrls("http://*:5000");
+                });
+    }
+}
---
+++ graduation_Remark/README.md
@@ -0,0 +1,18 @@
+# 关于项目的一些说明
+
+## 项目结构
+
+&emsp;&emsp;项目分为四部分：
+
+1. angular 部分，源码存放于 [graduation_Web 库](https://github.com/netLearner7/graduation_Web 'graduation_Web库') 中
+2. uni-app 部分，源码存放于 [graduation_APP 库](https://github.com/netLearner7/graduation_APP 'graduation_APP库') 中
+3. IdentityService4 部分，源码存放于 [graduation_IS4 库](https://github.com/netLearner7/graduation_IS4 'graduation_IS4库') 中
+4. API 部分，源码存放于 [graduation_API 库](https://github.com/netLearner7/graduation_API 'graduation_API库') 中
+
+## 测试用户
+
+&emsp;&emsp;系统提供两个默认账号为：alice 和 bob，默认密码为 Pass123\$。
+
+## App 包下载
+
+&emsp;&emsp;本库提供 App 的安装包，如果需要自行下载。包名：meeting_app.apk。
